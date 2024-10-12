da05dbe1568127a51485b6805d7853ff85d47e50
eclipse78 <132812659+eclipse78@users.noreply.github.com>
2024-10-12 14:11:09 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -116,7 +116,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(text="𝗝𝗼𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝟭", url=client.invitelink),
-            InlineKeyboardButton(text="𝗝𝗼𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝟮", url='https://t.me/+f4XAdMU1H3NmMjAx'),
+            InlineKeyboardButton(text="𝗝𝗼𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝟮", url='https://t.me/+GChhXc5YQsw0NDkx'),
         ],
         [
             InlineKeyboardButton(text="𝗝𝗼𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝟯", url=client.invitelink2),
